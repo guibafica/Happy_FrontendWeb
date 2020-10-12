d847b0918e30f006ec1e622d7f8b88fe4b2a3440
guibafica <gb.engcomp@hotmail.com>
2020-10-12 11:03:39 -0300

    ADD: Estilo global e landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import './styles/global.css';
+import './styles/pages/landing.css';
+
 function App() {
   return (
-    <div className="App">
+    <div id="page-landing">
       <h1>Hello World</h1>
     </div>
   );
 }
+// 1:06
 
 export default App;
